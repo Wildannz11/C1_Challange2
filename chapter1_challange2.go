1347d59c3760d6dde07991b65ea437f282407e79
wildannz11 <1910631170145@student.unsika.ac.id>
2023-03-12 11:49:35 +0700

    first commit

---
+++ chapter1_challange2.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main () {
+	for i := 0 ; i <= 4 ; i++ {
+		fmt.Println("Nilai i = ", i)
+	}
+
+	// keys := []string{"U+0421", "U+0410", "U+0428", "U+0410", "U+0420", "U+0412", "U+041E"}
+	keys := "CAШAPBO"
+    // values := []string{"C", "A", "Ⅲ", "A", "P", "B", "O"}
+
+	// unicode := make(map[string]string)
+
+    // for i := range keys {
+    //     unicode[keys[i]] = values[i]
+    // }
+	
+	p := 0
+	for j := 0 ; j <= 10; j++ {
+		if j == 5 {
+
+			for _, k := range keys {
+				// fmt.Printf("Character %v '%v' starts at byte position %v \n", k, k,p)
+				fmt.Printf("Character %#U starts at byte position %v \n", k, p)
+				p= p+2
+			}	
+			continue
+		}
+		
+		fmt.Println("Nilai j = ", j)
+	} 
+}
